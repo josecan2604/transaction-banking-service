103539abb5ee67ff6b17ee3d06009c00b52f0c59
Arana <Jose.Arana@pavilionpay.com>
2026-05-09 17:32:47 -0500

    Arana: Initial Commit, the following changes were added :
    Created initial project and scaffolding
    Added maven dependencies
    Created Initial Class for the app

---
+++ banking-transaction-service/src/main/java/org/banking/challenge/TransactionApplication.java
@@ -0,0 +1,12 @@
+package org.banking.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(TransactionApplication.class, args);
+        }
+    }
